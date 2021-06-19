97031b07444c782830cbfd1985356e6885704822
Fufutis <fufutis15@gmail.com>
2021-06-19 23:47:29 +0300

    some adjustments

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 // require the discord.js module
 const Discord = require('discord.js');
-
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 // create a new Discord client
 const client = new Discord.Client();
 
@@ -15,10 +14,18 @@ client.on('message', message => {
 	console.log(message.content);
 });*/
 client.on('message', message => {
-	if (message.content === '!ping') {
-		// send back "Pong." to the channel the message was sent in
-		message.channel.send('Pong.');
+	if (message.content === `${prefix}ping`) {
+		message.channel.send('Pong');
+	}
+	else if (message.content === `${prefix}beep`) {
+		message.channel.send('Boop');
+	}
+	else if (message.content === `${prefix}server`) {
+		message.channel.send(`DIS IS DA ${message.guild.name} DA ONE OF A KIND`);
+	}
+	else if (message.content === `${prefix}name`) {
+		message.channel.send(`YO NAME IS ${message.author.username}`);
 	}
 });
 // login to Discord with your app's token
-client.login('config.token');
+client.login(token);
