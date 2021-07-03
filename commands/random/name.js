e092c5f459a243bb7a81c91ce01fc74bad3b0b0c
Fufutis <fufutis15@gmail.com>
2021-07-04 00:32:56 +0300

    adjustments

--- commands/random/name.js
+++ commands/random/name.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'name',
 	description: '',
 	execute(message) {
-		message.channel.send(`YO NAME IS ${message.author.username}`);
+		message.channel.send(`YO NAME IS ${message.author.username}`.toUpperCase());
 	},
 };
--- commands/random/server.js
+++ commands/random/server.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'server',
 	description: 'just shows the name of your server',
 	execute(message) {
-		message.channel.send(`DIS IS DA ${message.guild.name} DA ONE OF A KIND`);
+		message.channel.send(`DIS IS DA ${message.guild.name} DA ONE OF A KIND`.toUpperCase());
 	},
 };
