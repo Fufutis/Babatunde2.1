e8272625744dad70f9af0420d5dd705e57b177f4
Fufutis <fufutis15@gmail.com>
2021-07-08 01:31:17 +0300

    help upgrade
    each command has its own explanation

--- commands/random/help.js
+++ commands/random/help.js
@@ -3,6 +3,26 @@ module.exports = {
 	aliases: ['commands'],
 	description: 'List all of my commands or info about a specific command.',
 	execute(message) {
-		message.channel.send('```Avatar,name,ping,roll,server,join ```');
+		if(message.content == 'baba help avatar') {
+			message.channel.send('asd');
+		}
+		else if(message.content == 'baba help name') {
+			message.channel.send('name');
+		}
+		else if(message.content == 'baba help ping') {
+			message.channel.send('ping');
+		}
+		else if(message.content == 'baba help roll') {
+			message.channel.send('roll');
+		}
+		else if(message.content == 'baba help server') {
+			message.channel.send('server');
+		}
+		else if(message.content == 'baba help join') {
+			message.channel.send('join');
+		}
+		else {
+			message.channel.send('```avatar,name,ping,roll,server,join```');
+		}
 	},
 };
